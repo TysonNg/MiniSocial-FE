cc8378ac3aa73d5c4a1df40ffd25066269b6498d
NGUYEN MINH NHAT <nhatnguyenminh1507@gmail.com>
2025-05-01 13:06:59 +0700

    fix

--- src/app/protected/protected.ts
+++ src/app/protected/protected.ts
@@ -7,12 +7,13 @@ export async function InstanceApi() {
   const cookieStore = await cookies();
   const userId = cookieStore.get("userId")?.value;
 
-  console.log('NEXT_PUBLIC_API_URL',process.env.NEXT_PUBLIC_API_URL);
-  console.log('api-url',process.env.API_URL);
+  console.log("NEXT_PUBLIC_API_URL", process.env.NEXT_PUBLIC_API_URL);
+  console.log("api-url", process.env.API_URL);
 
-  
+  const baseURL = process.env.API_URL ?? process.env.NEXT_PUBLIC_API_URL;
+  console.log("Final baseURL:", baseURL);
   const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL??process.env.API_URL,
+    baseURL: process.env.API_URL ?? process.env.NEXT_PUBLIC_API_URL,
     timeout: 10000,
     headers: {
       "Content-Type": "application/json",
@@ -20,58 +21,61 @@ export async function InstanceApi() {
     },
   });
 
-  api.interceptors.request.use((config) => {
-    const token = cookieStore.get('access_token')?.value
-    if(token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },async(err) => {
-    return Promise.reject(err)
-  }
-);
-
-  api.interceptors.response.use((res) => {
-    return res;
-  },async(err) => {
-    
-    const originalRequest = err.config
-    console.log('orinalll', err);
-    
-    if(err.response.status === 401 && err.response.statusText === 'Unauthorized'  && !originalRequest._retry){
-      originalRequest._retry = true
-
-      const refreshToken = cookieStore.get('refresh_token')?.value
-      
-      if(!refreshToken)
-      {
-        return Promise.reject(err)
-      }
-      try {
-          const res = await api.post('auth/refreshToken',{refreshToken})
-          
-          const newAccessToken = res.data.accessToken
-          const newRefreshToken = res.data.refreshToken
-          cookieStore.set('access_token', newAccessToken)
-          cookieStore.set('refresh_token', newRefreshToken)
-          return api(originalRequest)
-        } catch (error) {
-          cookieStore.delete('access_token')
-          cookieStore.delete('refresh_token')
-          cookieStore.delete('userId')
-          console.log(error);
-          
-          redirect('auth/signin')
-          
-      }
+  api.interceptors.request.use(
+    (config) => {
+      const token = cookieStore.get("access_token")?.value;
+      if (token) config.headers.Authorization = `Bearer ${token}`;
+      return config;
+    },
+    async (err) => {
+      return Promise.reject(err);
     }
-    return Promise.reject({
-      message: err?.response?.data?.message || "Something went wrong",
-      status: err?.response?.status || 500,
-      raw: err,
-    })
-  }
+  );
 
+  api.interceptors.response.use(
+    (res) => {
+      return res;
+    },
+    async (err) => {
+      const originalRequest = err.config;
+      console.log("orinalll", err);
 
-);
+      if (
+        err.response.status === 401 &&
+        err.response.statusText === "Unauthorized" &&
+        !originalRequest._retry
+      ) {
+        originalRequest._retry = true;
+
+        const refreshToken = cookieStore.get("refresh_token")?.value;
+
+        if (!refreshToken) {
+          return Promise.reject(err);
+        }
+        try {
+          const res = await api.post("auth/refreshToken", { refreshToken });
+
+          const newAccessToken = res.data.accessToken;
+          const newRefreshToken = res.data.refreshToken;
+          cookieStore.set("access_token", newAccessToken);
+          cookieStore.set("refresh_token", newRefreshToken);
+          return api(originalRequest);
+        } catch (error) {
+          cookieStore.delete("access_token");
+          cookieStore.delete("refresh_token");
+          cookieStore.delete("userId");
+          console.log(error);
+
+          redirect("auth/signin");
+        }
+      }
+      return Promise.reject({
+        message: err?.response?.data?.message || "Something went wrong",
+        status: err?.response?.status || 500,
+        raw: err,
+      });
+    }
+  );
 
   return api;
 }
